9853edd541e6573bab477b5b8549be5dd224d7ab
Yang Li <git@netcrop.org>
2020-06-18 07:11:46 +0000

    2020-06-18 15:11:46

--- src/conagent.py
+++ src/conagent.py
@@ -1,66 +1,71 @@
-#! /bin/env /bin/python3.8
-from sys import argv
-from glob import glob
-from time import sleep
-from datetime import datetime
-from getpass import getuser
-from socket import gethostname
-from random import randint
-from os import stat,getuid,environ,unlink,chmod,getenv,access,X_OK,W_OK,R_OK
-from subprocess import Popen, PIPE,call, run
-def genkey():
-    proc = run('tty',stdout=PIPE,text=True)
-    curtty = proc.stdout.rstrip('\n')
-    ttyuid = stat(curtty).st_uid
-    if ttyuid != uid:
-        print("User uid:",uid,"needs to became the owner of", curtty)
-        exit()
-    run(['mkdir','-p',sshdir])
-    tmpfh = open(tmpfile,'w')
-    proc = run(['pwgen','--capitalize','--numerals',
-    '--num-passwords=1','--secure'],stdout=tmpfh,stderr=PIPE)
-    environ['GPG_TTY'] = curtty
-    passfh = open(passasc,'w')
-    tmpfh = open(tmpfile,'r')
-    proc = run(['gpg','--symmetric','--no-verbose','--quiet',
-    '--armor'],stdin=tmpfh,stdout=passfh,stderr=PIPE)
-    chmod(tmpfile,0o700)
-    tmpfh = open(tmpfile,'w')
-    print ("""#!/bin/env /bin/bash
-builtin declare -x GPG_TTY=%s 
-gpg --decrypt --no-verbose --quiet %s"""%(curtty,passasc),file=tmpfh)
-    tmpfh.close()
-    environ['DISPLAY'] = ':0'
-    environ['SSH_ASKPASS'] = tmpfile
-    nullfh = open('/dev/null','r')
-    cmd = ['ssh-keygen','-C',userhost ,'-t', keytype,'-f', keyfile]
-    proc = run(cmd,stdin=nullfh)
-    files = ' '.join(glob(sshdir + '*'))
-    cmd = 'chmod u=r,go= ' + files
-    run(cmd.split())
-    cmd = 'cp -av ' + files + ' ' + backupdir
-    run(cmd.split()) 
+#!/bin/env -S PATH=/usr/local/bin:/usr/bin /bin/python3.8 -I
+from subprocess import *
+import glob,io,subprocess,sys,os,socket,getpass,random,datetime
+class Conagent:
+    def __init__(self,*argv):
+        self.argv = argv
+        self.keytype = 'rsa'
+        self.hostname = socket.gethostname()
+        self.uid = os.getuid()
+        self.username = getpass.getuser()
+        self.userhost = self.username + '@' + self.hostname
+        self.homedir = os.environ.get('HOME')
+        self.sshdir = self.homedir + '/tmp/'
+        self.tmpfile = '/var/tmp/' + str(random.randint(10000,99999))
+        self.date = datetime.datetime.utcnow().strftime("%Y%m%d%H%M%S")
+        self.keyfile = self.sshdir + self.username + '_' + self.hostname + '_' + \
+        self.keytype + '_' + self.date
+        self.passasc = self.keyfile + '_pass.asc'
+    def genkey(self):
+        try:
+            if len(self.argv[0]) != 2:
+                print(self.argv[0][0],'genkey [backup dir]')
+                return
+            self.backupdir = self.argv[0][1] 
+            if not (os.access(self.backupdir,os.X_OK) 
+            and os.access(self.backupdir,os.W_OK)):
+                return
+            self.proc = run('tty',stdout=PIPE,text=True,check=True)
+            self.curtty = self.proc.stdout.rstrip('\n')
+            self.ttyuid = os.stat(self.curtty).st_uid
+            if self.ttyuid != self.uid:
+                print("User uid:",self.uid,"needs to became the owner of",self.curtty)
+                return
+            run(['mkdir','-p',self.sshdir],check=True)
+            self.tmpfh = open(self.tmpfile,'w+')
+            run(['pwgen','--capitalize','--numerals',
+            '--num-passwords=1','--secure'],stdout=self.tmpfh,check=True)
+            os.environ['GPG_TTY'] = self.curtty
+            self.passfh = open(self.passasc,'w')
+            run(['gpg','--symmetric','--no-verbose','--quiet','--armor'],
+            stdin=self.tmpfh,stdout=self.passfh,check=True)
+            os.chmod(self.tmpfile,0o700)
+            self.tmpfh = open(self.tmpfile,'w')
+            self.script = ("""\
+            #!/bin/env /bin/bash
+            builtin declare -x GPG_TTY=%s
+            /bin/gpg --decrypt --no-verbose --quiet %s""")
+            print( self.script.replace("    ","") % (self.curtty,self.passasc),
+            file=self.tmpfh)
+            self.tmpfh.close()
+            os.environ['DISPLAY'] = ':0'
+            os.environ['SSH_ASKPASS'] = self.tmpfile
+            self.nullfh = open('/dev/null','r')
+            run(['ssh-keygen','-C',self.userhost,'-t',self.keytype,'-f',self.keyfile],
+            check=True,stdin=self.nullfh)
+            self.files = ' '.join(glob.glob(self.sshdir + '*'))
+            self.cmd = 'chmod u=r,go= ' + self.files
+            run(self.cmd.split(),check=True);
+            self.cmd = 'cp -av ' + self.files + ' ' + self.backupdir
+            run(self.cmd.split())
+        finally:
+#            print(agent.__dict__)
+            for i in agent.__dict__.values():
+                if isinstance(i,io.TextIOWrapper):
+                    i.close()
+            if ( os.access(self.tmpfile,os.R_OK) and os.access(self.tmpfile,os.W_OK) ):
+                os.unlink(self.tmpfile)
+                print('finally')
 if __name__ == '__main__':
-    if len(argv) != 2:
-        print(argv[0],'[backup dir]')
-        exit()
-    backupdir = argv[1]
-    if not (access(backupdir,X_OK) and access(backupdir,W_OK)):
-        print(backupdir,'inaccessible')
-        exit()
-    keytype = 'rsa'
-    hostname = gethostname()
-    uid = getuid()
-    username = getuser()
-    userhost = username + '@' + hostname
-    homedir = environ.get('HOME')
-    sshdir = homedir+'/tmp/'
-    tmpfile = '/var/tmp/' + str(randint(10000,99999))
-    date = datetime.utcnow().strftime("%Y%m%d%H%M%S")
-    keyfile = sshdir + username + '_' + hostname + '_' + keytype + '_' + date
-    passasc = keyfile + '_pass.asc'
-    try:
-        genkey()
-    finally:
-        if (access(tmpfile,R_OK) and access(tmpfile,W_OK)):
-            unlink(tmpfile)
+    agent = Conagent(sys.argv)
+    agent.genkey()
